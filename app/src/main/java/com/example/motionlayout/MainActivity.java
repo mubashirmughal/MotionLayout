a99286a9c0efaccdd7095bffea73ab111e529c77
Muhammad Mubashir Mughal <donleo26@gmail.com>
2023-01-30 22:07:35 -0500

    index

---
+++ app/src/main/java/com/example/motionlayout/MainActivity.java
@@ -0,0 +1,31 @@
+package m.ify.twitter;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.motion.widget.MotionLayout;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(MainActivity.this,MainActivity2.class));
+                finish();
+            }
+        },3000);
+
+
+    }
+}
